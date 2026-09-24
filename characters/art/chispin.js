3096a9c72afc4a5e462a20ee73d6f16952afe24a
Gracián Baena <108979617+GracianB@users.noreply.github.com>
2026-09-24 20:47:25 +0200

    Ohana pass: foes factory complete, foe-rig, full roster, docs

---
+++ characters/art/chispin.js
@@ -0,0 +1,1 @@
+export { default } from "./pikachu.js";
--- characters/art/index.js
+++ characters/art/index.js
@@ -1,11 +1,16 @@
 // Registro de personajes vectoriales (id del roster → módulo de arte)
-import lilo from "./lilo.js";
+import kilo from "./kilo.js";
 import stitch from "./stitch.js";
-import pikachu from "./pikachu.js";
+import chispin from "./chispin.js";
 import cat from "./cat.js";
 import dragon from "./dragon.js";
 import dino from "./dino.js";
 import frita from "./frita.js";
 import pizza from "./pizza.js";
 
-export const ART = { lilo, stitch, pikachu, cat, dragon, dino, frita, pizza };
+export const ART = {
+  lilo: kilo, kilo,
+  stitch,
+  pikachu: chispin, chispin,
+  cat, dragon, dino, frita, pizza
+};
